f17cfd5291a19060154ca4aeb5e1886c3ba91a89
JJ07701 <jangraj845@gmail.com>
2025-10-09 07:22:09 +0530

    Create 70.c

---
+++ 70.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    printf("Enter number of elements in array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter number of positions to rotate (k): ");
+    scanf("%d", &k);
+    k = k % n;
+    for (int i = 0, j = n - 1; i < j; i++, j--) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    for (int i = 0, j = k - 1; i < j; i++, j--) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    for (int i = k, j = n - 1; i < j; i++, j--) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    printf("Array after rotating right by %d positions:\n", k);
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
