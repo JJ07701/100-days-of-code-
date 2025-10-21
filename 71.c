e884ef3b4c7b3f7547ce523bd31b3680da9c9ed1
JJ07701 <jangraj845@gmail.com>
2025-10-21 13:58:46 +0530

    Create 71.c

---
+++ 71.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    int matrix[10][10];  
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i + 1, j + 1);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    printf("\nThe matrix is:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
